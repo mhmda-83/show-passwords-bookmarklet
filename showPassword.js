bde0589942af4925e6f4f8c89a0843ab95e4d39e
Mohammad MohammadAlian <mralian.dev@gmail.com>
2020-03-22 22:27:28 +0430

    better way to check shownPasswords!

--- showPassword.js
+++ showPassword.js
@@ -1,59 +1,13 @@
-function previousElementSibling(element) {
-  if (element.previousElementSibling !== "undefined") {
-    return element.previousElementSibling;
-  } else {
-    while ((element = element.previousSibling)) {
-      if (element.nodeType === 1) {
-        return element;
-      }
-    }
+let shownPasswords = document.querySelectorAll('input[data-is-password-field]');
+if (shownPasswords.length != 0) {
+  for (let i = 0; i < shownPasswords.length; i++) {
+    shownPasswords[i].type = 'password';
+    delete shownPasswords[i].dataset.isPasswordField;
   }
-}
-function getPath(element) {
-  if (!(element instanceof HTMLElement)) {
-    return false;
-  }
-  var path = [];
-  while (element.nodeType === Node.ELEMENT_NODE) {
-    var selector = element.nodeName;
-    if (element.id) {
-      selector += "#" + element.id;
-    } else {
-      var sibling = element;
-      var siblingSelectors = [];
-      while (sibling !== null && sibling.nodeType === Node.ELEMENT_NODE) {
-        siblingSelectors.unshift(sibling.nodeName);
-        sibling = previousElementSibling(sibling);
-      }
-      if (siblingSelectors[0] !== "HTML") {
-        siblingSelectors[0] = siblingSelectors[0] + ":first-child";
-      }
-      selector = siblingSelectors.join(" + ");
-    }
-    path.unshift(selector);
-    element = element.parentNode;
-  }
-  return path.join(" > ");
-}
-
-if (document.body.dataset.passwordsElementSelector) {
-  let passwordInputs = JSON.parse(
-    document.body.dataset.passwordsElementSelector
-  );
-  for (var i = 0; i < passwordInputs.length; i++) {
-    document.querySelector(passwordInputs[i]).type = "password";
-  }
-  delete document.body.dataset.passwordsElementSelector;
 } else {
-  var passwordsElementSelectors = [];
-
-  var passwordElements = document.querySelectorAll('input[type="password"]');
-  for (var i = 0; i < passwordElements.length; i++) {
-    passwordElements[i].type = "text";
-    passwordsElementSelectors.push(getPath(passwordElements[i]));
+  let passwordElements = document.querySelectorAll('input[type="password"]');
+  for (let i = 0; i < passwordElements.length; i++) {
+    passwordElements[i].type = 'text';
+    passwordElements[i].dataset.isPasswordField = true;
   }
-
-  document.body.dataset.passwordsElementSelector = JSON.stringify(
-    passwordsElementSelectors
-  );
 }
